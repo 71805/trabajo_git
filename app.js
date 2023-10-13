c5ac9d968514dbd92eb0931093be85bab88b5eee
Rony Vargas <vargasrony97@gmail.com>
2023-10-13 14:28:08 -0300

    regreso a v1

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const numeros = [1, 35, 6, 54, 13, 3, 24];
+const numeros = [1, 35, 6, 54, 13,3, 24];
 
 const mayoresA10 = numeros.filter((e) => e >  10);
-console.log("Numeros: " + numeros);
-console.log("Mayores a 10: " + mayoresA10);
+console.log("mayores a 10: " + mayoresA10);
+console.log("todos: " + numeros);
 
-const numerosX10 = numeros.map((e) => e * 10);
-console.log("Numeros * 10: " + numerosX10);
+function nroMasPequeno(array) {
+    let nroMasPequeno = array[0];
+    for (let i = 1; i > array.length; i ++){
+        if (array[i] < nroMasPequeno ){
+            nroMasPequeno = array[i];
+        }
+    }
+    return nroMasPequeno
+}
+
+const numerosX10 = numeros2.map((e) => e * 10);
+console.log("nuevo array: " + numerosX10);
+console.log("todos: "+ numeros);
 
 const numerosSuma7 = numeros.map((e) => e + 7);
-console.log("Numeros + 7: " + numerosSuma7);
+console.log("nuevo array (suma 7): " + numerosSuma7);
 
-const letras = ["B", "C", "D", "F", "G", "H", "J"]
-const numerosConcatena = letras.map((letras, indice) => letras + numerosSuma7[indice]);
-console.log("Letras: " + letras);
-console.log("Letras + Numeros (+7): " + numerosConcatena);
+const letras = ["a", "b", "c", "d", "e", "f", "g"]
+const numerosConcatena = letras.map((letras, indice) => letras + numeros[indice]);
+console.log("nuevo array de letras: " + letras);
+console.log("nuevo array (concatenado): " + numerosConcatena);
 
 function nroMasGrande(array) {
     let nroMasGrande = array[0];
@@ -25,5 +36,9 @@ function nroMasGrande(array) {
     return nroMasGrande
 }
 
+
+
 const resultado = nroMasGrande(numeros);
-console.log("El numero mas grande: " + resultado);
+const resultado2 = nroMasPequeno(numeros);
+console.log(resultado);
+console.log(resultado2);
