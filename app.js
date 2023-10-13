62af453b51984ea3c380d2974c6bf62606ba792f
Rony Vargas <vargasrony97@gmail.com>
2023-10-13 14:18:27 -0300

    Funcion producto de todos los numeros

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const numeros = [1, 35, 6, 54, 13,3, 24];
 
+const productoTotal = numeros.reduce((acumulador, numero) => acumulador * numero, 1);
+console.log("Producto de todos los numeros: " + productoTotal);
+
 const mayoresA10 = numeros.filter((e) => e >  10);
 console.log("mayores a 10: " + mayoresA10);
 console.log("todos: " + numeros);
@@ -8,7 +11,7 @@ const numerosX10 = numeros.map((e) => e * 10);
 console.log("nuevo array: " + numerosX10);
 console.log("todos: "+ numeros);
 
-const letras = ["B", "C", "D", "F", "G", "H", "J"]
+const letras = ["B", "C", "D", "F", "G", "H", "J"];
 const numerosConcatena = letras.map((letras, indice) => letras + numeros[indice]);
 console.log("nuevo array de letras: " + letras);
 console.log("nuevo array (concatenado): " + numerosConcatena);
