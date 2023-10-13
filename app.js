040ab15bee3a481939dc866dec0b52e85f10dbdc
Rony Vargas <vargasrony97@gmail.com>
2023-10-13 14:41:02 -0300

    version 3?

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-const numeros = [1, 35, 6, 54, 13,3, 24];
+const numeros = [1, 35, 6, 54, 13, 3, 24];
+
+const productoTotal = numeros.reduce((acumulador, numero) => acumulador * numero, 1)
+console.log("Producto de todos los numeros: " + productoTotal);
 
 const mayoresA10 = numeros.filter((e) => e >  10);
-console.log("mayores a 10: " + mayoresA10);
-console.log("todos: " + numeros);
+console.log("Numeros: " + numeros);
+console.log("Mayores a 10: " + mayoresA10);
 
-function nroMasPequeno(array) {
-    let nroMasPequeno = array[0];
-    for (let i = 1; i > array.length; i ++){
-        if (array[i] < nroMasPequeno ){
-            nroMasPequeno = array[i];
-        }
-    }
-    return nroMasPequeno
-}
-
-const numerosX10 = numeros2.map((e) => e * 10);
-console.log("nuevo array: " + numerosX10);
-console.log("todos: "+ numeros);
+const numerosX10 = numeros.map((e) => e * 10);
+console.log("Numeros * 10: " + numerosX10);
 
 const numerosSuma7 = numeros.map((e) => e + 7);
-console.log("nuevo array (suma 7): " + numerosSuma7);
+console.log("Numeros + 7: " + numerosSuma7);
 
 const letras = ["a", "b", "c", "d", "e", "f", "g"]
 const numerosConcatena = letras.map((letras, indice) => letras + numeros[indice]);
@@ -36,9 +28,5 @@ function nroMasGrande(array) {
     return nroMasGrande
 }
 
-
-
 const resultado = nroMasGrande(numeros);
-const resultado2 = nroMasPequeno(numeros);
-console.log(resultado);
-console.log(resultado2);
+console.log("El numero mas grande: " + resultado);
