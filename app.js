cddeb730db406ca7235e8c4df3d613d456373d38
Rony Vargas <vargasrony97@gmail.com>
2023-10-14 20:44:05 -0300

    Letras y Concatenado

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ console.log("todos: " + numeros);
 const numerosX10 = numeros.map((e) => e * 10);
 console.log("nuevo array: " + numerosX10);
 console.log("todos: "+ numeros);
+
+const letras = ["a", "b", "c", "d", "e", "f", "g"]
+const numerosConcatena = letras.map((letras, indice) => letras + numeros[indice]);
+console.log("nuevo array de letras: " + letras);
+console.log("nuevo array (concatenado): " + numerosConcatena);
