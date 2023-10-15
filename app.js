590a81cc42fef1cfa83cdb4e4667d1bc1204935f
Camila Chavez <71805@sistemas.frc.utn.edu.ar>
2023-10-15 02:01:47 -0300

    se agrego una funcion nueva

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const mayoresA10 = numeros.filter((e) => e >  10);
 console.log("mayores a 10: " + mayoresA10);
 console.log("todos: " + numeros);
 
+function nroMasPequeno(array) {
+    let nroMasPequeno = array[0];
+    for (let i = 1; i > array.length; i ++){
+        if (array[i] < nroMasPequeno ){
+            nroMasPequeno = array[i];
+        }
+    }
+    return nroMasPequeno
+}
+
 const numerosX10 = numeros2.map((e) => e * 10);
 console.log("nuevo array: " + numerosX10);
 console.log("todos: "+ numeros2);
@@ -24,5 +34,9 @@ function nroMasGrande(array) {
     return nroMasGrande
 }
 
+
+
 const resultado = nroMasGrande(numeros);
+const resultado2 = nroMasPequeno(numeros);
 console.log(resultado);
+console.log(resultado2);
